31d49d4e27456d9d849aa08bf63ab3538395f272
qibin <qibinchen1994@gmail.com>
2022-01-01 05:36:04 +0100

    Add RGB features

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ function clearContainer() {
     while(container.firstElementChild) container.removeChild(container.lastElementChild);
 };
 
-function changeColor(node, color) {
-    node.style['background-color'] = color; 
+function changeColor(node) {
+    const hue = Math.floor(Math.random()*361);
+    lightness -= 10;
+    if(lightness < 0) lightness = 90; // Reset lightness every 10 times
+    node.style['background-color'] = 'hsl(' + hue + ',' + '100%,' + lightness + '%)'; 
 };
 
 function createGrid(length) {
@@ -17,20 +20,23 @@ function createGrid(length) {
         const div = document.createElement('div');
         div.setAttribute('class', 'grid-item');
         container.appendChild(div);
+        div.addEventListener('mouseenter', function() {changeColor(this)});
     };
 
-    const gridItems = document.querySelectorAll('.grid-item');
-    gridItems.forEach(gritItem => 
-        gritItem.addEventListener('mouseenter', function() {changeColor(this, 'blue')}));
+    // Grid layout
     container.style['grid-template-columns'] = `repeat(${length}, 1fr)`;
 };
 
 function reset() {
     sideLength = prompt('Enter the numbers of squares per side: (max: 64):');
     while(sideLength > 64) sideLength = prompt('Too large! Please enter a number no larger than 64:');
+    lightness = 100;
     createGrid(sideLength);
 };
 
-createGrid(sideLength); // Initialize
+ // Initialize
+let lightness = 100;
+createGrid(sideLength);
+
 const resetBtn = document.querySelector('#reset-btn');
 resetBtn.addEventListener('click', reset);
